71556d14f614158086375c36ca560d52c93fe0f7
joeyharbert <joey.harbert@gmail.com>
2019-02-27 20:02:47 -0600

    Makes guessing game dynamic with global variables

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -12,17 +12,25 @@ class Api::GamesController < ApplicationController
     render 'name.json.jbuilder'
   end
 
+  @@num = nil
   def guess
-    num = 36
+    if params[:guess] == nil
+       @@num = rand(1..100)
+      # p "*" * 80
+      # p @@num
+      # p "*" * 80    #test code
+     end 
+
+    #num = 36
     @user_guess = params[:guess].to_i
 
     if @user_guess <= 0 || @user_guess > 100 
       @message = "Please guess a number between 1-100"
-    elsif @user_guess == num
+    elsif @user_guess == @@num
       @message = "Correct!"
-    elsif @user_guess > num
+    elsif @user_guess > @@num
       @message = "Too high! Guess again."
-    elsif @user_guess < num
+    elsif @user_guess < @@num
       @message = "Too low! Guess again."
     end
     render 'guess.json.jbuilder'
