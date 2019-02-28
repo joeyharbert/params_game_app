3187916a7cd49ef9d3d643677c390d41562b0888
joeyharbert <joey.harbert@gmail.com>
2019-02-27 19:42:18 -0600

    Adds logic so site will tell you if guess was too low or high (or asks you to stay within the parameters if outside the range)

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -16,12 +16,14 @@ class Api::GamesController < ApplicationController
     num = 36
     @user_guess = params[:guess].to_i
 
-    if @user_guess == nil
+    if @user_guess <= 0 || @user_guess > 100 
       @message = "Please guess a number between 1-100"
     elsif @user_guess == num
       @message = "Correct!"
-    else
-      @message = "Guess again."
+    elsif @user_guess > num
+      @message = "Too high! Guess again."
+    elsif @user_guess < num
+      @message = "Too low! Guess again."
     end
     render 'guess.json.jbuilder'
   end
